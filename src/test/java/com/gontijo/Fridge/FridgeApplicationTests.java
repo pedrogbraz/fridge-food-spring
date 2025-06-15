2f1f746b757c789ecf3228b78c48bceef2a642e3
pedrogbraz <pedrobrazdevs@gmail.com>
2025-06-15 20:59:58 -0300

    feat: add inicialization configs

---
+++ src/test/java/com/gontijo/Fridge/FridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gontijo.Fridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
