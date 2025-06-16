361a986fab4986f2495849051e0d9d976adf6ea9
pedrogbraz <pedrobrazdevs@gmail.com>
2025-06-15 21:32:48 -0300

    feat(controller): add initial food controller

--- src/main/java/com/gontijo/Fridge/controller/FoodController.java
+++ src/main/java/com/gontijo/Fridge/controller/FoodController.java
@@ -1,4 +1,29 @@
 package com.gontijo.Fridge.controller;
 
+import com.gontijo.Fridge.model.Food;
+import com.gontijo.Fridge.service.FoodService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/food")
 public class FoodController {
+    // Dependency injection
+    private final FoodService foodService;
+
+    public FoodController(FoodService foodService) {
+        this.foodService = foodService;
+    }
+
+    @GetMapping
+    public List<Food> getFoods() {
+        return foodService.getFoods();
+    }
+
+    @PostMapping
+    public Food create(@RequestBody Food food) {
+        return foodService.save(food);
+    }
 }
