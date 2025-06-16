c4f6fb0d887a11413cc137412d1ba31026cb0508
pedrogbraz <pedrobrazdevs@gmail.com>
2025-06-15 21:33:24 -0300

    feat(service): add initial service

--- src/main/java/com/gontijo/Fridge/service/FoodService.java
+++ src/main/java/com/gontijo/Fridge/service/FoodService.java
@@ -1,4 +1,32 @@
 package com.gontijo.Fridge.service;
 
+import com.gontijo.Fridge.model.Food;
+import com.gontijo.Fridge.repository.FoodRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class FoodService {
+
+    private final FoodRepository foodRepository;
+
+    public FoodService(FoodRepository foodRepository) {
+        this.foodRepository = foodRepository;
+    }
+
+    // Listar todos
+    public List<Food> getFoods() {
+        return foodRepository.findAll();
+    }
+
+    // Criar um novo alimento
+    public Food save(Food food) {
+        return foodRepository.save(food);
+    }
+
+    // Deletar um alimento
+    public void delete(Long id) {
+        foodRepository.deleteById(id);
+    }
 }
